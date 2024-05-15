5cb5351afc1e3ad517a567c957d1f61ddb70e323
Chidi G N <gnwankpa@gmail.com>
2024-05-15 16:20:48 -0500

    cleanup

--- archive/currency-convert.js
+++
@@ -1,6 +0,0 @@
-// USD CAD 23
-// 23 USD is wrth 28 CAD. You can spend these in the following countries:
-
-// https://api.fixer.io/latest?base=USD
-// https://restcountries.eu/rest/v2/currency/usd
-// https://restcountries.eu/rest/v2/currency/cad
--- archive/nullCondition.js
+++
@@ -1,9 +0,0 @@
-let x = 1
-let y = {b:1}
-
-
-if(y??null){
-    console.log('true')
-}else{
-    console.log('false')
-}
